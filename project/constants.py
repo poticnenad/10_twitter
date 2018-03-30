1d1c0fd0d791baf719196cc69e877d611cdc168d
Nenad Potic <nenad@debian>
2018-03-30 17:03:55 +0200

    Initially upload aproject

---
+++ project/constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = '4cbk7w0rxHUx7uNiWaUySEmGv'
+CONSUMER_SECRET = 'aOXNcf1gAysA16ck2Wf9EDAf1SQT0A1mq4fahzNDO2a1giJ9He'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
